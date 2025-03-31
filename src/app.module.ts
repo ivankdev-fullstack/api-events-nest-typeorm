c469ba3d6246c513d28de663e373d2a023b75b7e
Ivan Kononenko <ivan.kononenko.dev@gmail.com>
2025-03-31 18:15:44 +0300

    fix: fix PORT issue v2

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
-import { appConfig } from './config/app.config';
 import { authConfig } from './config/auth.config';
 import { typeOrmConfig } from './config/database.config';
 import { TypedConfigService } from './config/typed-config.service';
@@ -16,12 +15,12 @@ import { UserModule } from './user/user.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      load: [appConfig, typeOrmConfig, authConfig],
+      load: [typeOrmConfig, authConfig],
       expandVariables: true,
       envFilePath: `${process.env.NODE_ENV}.env`,
       validationSchema: appConfigSchema,
       validationOptions: {
-        abortEarly: true,
+        abortEarly: false,
       },
     }),
     TypeOrmModule.forRootAsync({
