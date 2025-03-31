60c1dbdfb8bfc771cb7b3b23cd964759fd8d18b7
Ivan Kononenko <ivan.kononenko.dev@gmail.com>
2025-03-31 19:33:33 +0300

    chore: fix envs

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,21 +4,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeOrmConfig = registerAs(
   'database',
   (): TypeOrmModuleOptions => {
-    console.log();
-    console.log('==================================');
-    console.log();
-    console.log(process.env.DATABASE_URL);
-    console.log(process.env.DATABASE_NAME);
-    console.log(process.env.DB_SYNC);
-    console.log(process.env.JWT_SECRET);
-    console.log();
-    console.log('==================================');
-    console.log();
     return {
       type: 'postgres',
       url: process.env.DATABASE_URL,
       database: process.env.DATABASE_NAME,
-      synchronize: Boolean(parseInt(process.env.DB_SYNC ?? '0')),
+      synchronize: Boolean(parseInt(process.env.DATABASE_SYNC ?? '0')),
     };
   },
 );
